29494e825cd66c74481e63d0581ea181839f19bc
Alex <Alex.Iskander@Gmail.com>
2022-05-10 10:06:08 -0700

    Upload initial data

---
+++ idm/idm_token_status.sql
@@ -0,0 +1,4 @@
+INSERT INTO idm.token_status (id, value)
+VALUES  (1, 'Active'),
+        (2, 'Expired'),
+        (3, 'Revoked');
---
+++ idm/idm_user_status.sql
@@ -0,0 +1,4 @@
+INSERT INTO idm.user_status (id, value)
+VALUES  (1, 'Active'),
+        (2, 'Locked'),
+        (3, 'Banned');
